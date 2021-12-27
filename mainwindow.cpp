5d92bf8dcc2ff9999fcaeae121cf4dd84d499d57
izyben <ahmadbader245@gmail.com>
2021-12-27 18:07:37 +0200

    Second commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -48,6 +48,7 @@ void MainWindow::on_pushButton_clicked()
 {
     MYSQL *con = NULL;
     MYSQL_RES *res;
+    MYSQL_ROW row = NULL;
     struct connection_details mysqlD;
     mysqlD.server = "localhost";
     mysqlD.user = "root";
@@ -55,12 +56,44 @@ void MainWindow::on_pushButton_clicked()
     mysqlD.database = "universityDatabase";
     //QMessageBox msgbox;
     con = mysql_connection_setup(mysqlD);
-    QMessageBox msgbox;
-    /*if(con != NULL){
-        msgbox.setText("Successful");
-        msgbox.exec();
-    }
-    else
-        msgbox.setText("Failed");*/
+    QString SId = ui->userIDLogin->text();
+    //std::cout<<SId;
+    QString password = ui->passwordLogin->text();
+    mysql_query(con, "select ID from student");
+    res = mysql_store_result(con);
 
+    QMessageBox loginMessage;
+    while((row = mysql_fetch_row(res)) != NULL){
+        std::string r = row[0];
+        QString qrow = QString::fromStdString(r);
+       std::cout<<r<<std::endl;
+       if(qrow == SId){
+           loginMessage.setText("Login successful");
+           std::cout<<"True";
+           break;
+
+       }
+        else
+           loginMessage.setText("Login unsuccessful");
+    }
+    loginMessage.exec();
+    /* QMessageBox box;
+    if(row == NULL){
+        box.setText("Username or password was incorrect");
+    }
+    else{
+        box.setText("Successful login");
+        box.setText(row[0]);
+        for(int i = 0; i < 5; i++){
+            std::cout<<row[i];
+        }
+    }
+        box.exec();
+    int rows = mysql_num_rows(res);
+    for(int i = 0; i < rows; i++){
+
+    }
+    row = NULL;*/
+    mysql_free_result(res);
+    // mysql_close(con);
 }
